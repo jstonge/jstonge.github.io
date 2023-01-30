ec3e433b35724ae3360087b3bbe8162be60cf7eb
jstonge <jonathanstonge7@gmail.com>
2023-01-30 09:34:28 -0500

    update res as individual file

--- posts/source-sink/src/04_processing.jl
+++ posts/source-sink/src/04_processing.jl
@@ -1,5 +1,5 @@
 using Pkg; Pkg.activate("../..");
-using SQLite, DataFrames, CSV, ProgressMeter, ArgParse
+using DataFrames, CSV, ProgressMeter, ArgParse, Parquet
 
 function parse_commandline()
   s = ArgParseSettings()
@@ -39,12 +39,12 @@ function main()
 
   @assert length(fnames) > 0 println("There are no data files at the given directory")
 
-  db = SQLite.DB("source-sink-res.db")
+  # db = SQLite.DB("source-sink-res.db")
   modelname = split(fnames[1], "_")[1]
   
-  create_res_db(db, modelname)
+  # create_res_db(db, modelname)
   #!TODO: Change to correct name
-  already_done = DBInterface.execute(db, """SELECT DISTINCT name FROM $(modelname)""") |> DataFrame
+  # already_done = DBInterface.execute(db, """SELECT DISTINCT name FROM $(modelname)""") |> DataFrame
 
   dfs = []
   @showprogress for fname in fnames
@@ -52,24 +52,25 @@ function main()
     fname_parts = split(fname, "/")
     fname = fname_parts[end]
     p_str = replace(join(split(fname, "_")[2:end], "_"), ".txt" => "")
-    set_already_done = Set(already_done.name)
-    if !(p_str in set_already_done)
+    # set_already_done = Set(already_done.name)
+    # if !(p_str in set_already_done)
       
-      gd = groupby(sol, [:timestep, :L])
-      n = nrow(gd[1])
-      
-      # df_agg_mean = combine(gd, :value => x -> iszero(sum(x)) ? 0.0 : mean(x,n)) 
-      df_agg = combine(gd, :value => x -> iszero(sum(x)) ? 0.0 : processing1(x,n))
-      rename!(df_agg, Dict(:value_function => "value")) 
-      unique!(df_agg, :value)
+    gd = groupby(sol, [:timestep, :L])
+    n = nrow(gd[1])
+    
+    # df_agg_mean = combine(gd, :value => x -> iszero(sum(x)) ? 0.0 : mean(x,n)) 
+    df_agg = combine(gd, :value => x -> iszero(sum(x)) ? 0.0 : processing1(x,n))
+    rename!(df_agg, Dict(:value_function => "value")) 
+    unique!(df_agg, :value)
 
-      df_agg[!, :name] .= p_str
-      push!(dfs, df_agg)      
-    end
+    df_agg[!, :name] .= p_str
+    push!(dfs, df_agg)      
+    # end
   end
 
-    all_dfs = vcat(dfs...) 
-    all_dfs |> SQLite.load!(db, "$(modelname)") 
+  all_dfs = vcat(dfs...) 
+  # all_dfs |> SQLite.load!(db, "$(modelname)")
+  write_parquet("$(modelname).parquet", all_dfs)
 
 end
 
